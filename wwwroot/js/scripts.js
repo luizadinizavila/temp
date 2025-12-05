c1577aacfc6a8f0f976576e627800209b03d01d4
luizadinizavila <luiza@aviladigital.com.br>
2025-12-05 14:44:32 -0300

    .

--- wwwroot/js/scripts.js
+++
@@ -1,6 +0,0 @@
-//var modalAlert = new bootstrap.Modal(document.getElementById("modalArlet"));
-//modalAlert.show();
-
-function showModal() {
-    $('#myModal').modal('show'); // Isso abre o modal com a classe "fade"
-}
